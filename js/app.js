73b34ae43c40853acd2e5f0dbd7807a0673aef4a
Korawit Prutsachainimmit <korawit.p@gmail.com>
2020-09-30 00:35:38 +0700

    Add: Sign in with email and google

--- js/app.js
+++ js/app.js
@@ -1,12 +1,75 @@
 // App logic.
-window.myApp = {};
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    // var displayName = user.displayName;
+    var email = user.email;
+    // var emailVerified = user.emailVerified;
+    // var photoURL = user.photoURL;
+    // var isAnonymous = user.isAnonymous;
+    // var uid = user.uid;
+    // var providerData = user.providerData;
 
-document.addEventListener('init', function(event) {
-  var page = event.target;
-
-  // Each page calls its own initialization controller.
-  if (myApp.controllers.hasOwnProperty(page.id)) {
-    myApp.controllers[page.id](page);
+    console.log(email);
+    document.querySelector('#myNavigator').replacePage('tabs.html');
+  } else {
+    document.querySelector('#myNavigator').replacePage('views/signin.html');
   }
+});
+
+
+
+document.addEventListener('init', function (event) {
+  var page = event.target;
+  console.log(page.id);
+
+  document.getElementById("signout").onclick = function () {
+    firebase.auth().signOut().then(function () {
+      // Sign-out successful.
+    }).catch(function (error) {
+      // An error happened.
+    });
+  }
+
+
+  if (page.id === "home") {
+    db.collection("movies").get().then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        var item = `<ons-carousel-item>
+        <div style="text-align: center;">
+          <img src="${doc.data().posterURL}" style="width:100%;">
+        </div>
+      </ons-carousel-item>`
+        var onsItem = document.createElement('ons-carousel-item');
+        onsItem.innerHTML = item;
+        page.querySelector('#carousel').appendChild(onsItem);
+      });
+    });
+
+  }
+
+  if (page.id === "signin") {
+    document.getElementById("signinbutton").onclick = function () {
+      var username = document.querySelector("#username").value;
+      var password = document.querySelector("#password").value;
+      console.log(username, password);
+
+      firebase.auth().signInWithEmailAndPassword(username, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        document.querySelector("#error").innerHTML = errorMessage;
+      });
+    }
+
+    document.getElementById("signingoogle").onclick = function () {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+    }
+  }
+
 
 });
